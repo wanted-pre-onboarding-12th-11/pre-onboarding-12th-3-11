81fc5e6046d023f60a76489f9a11375b7ad2a0a5
Taekjin Jang <xorwls1500@naver.com>
2023-09-05 20:04:03 +0900

    init: 초기세팅 및 코드컨벤션 적용

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// .prettierrc.json
+
+module.exports = {
+    singleQuote: true,
+    endOfLine: 'lf',
+    trailingComma: 'es5',
+    bracketSpacing: false,
+    jsxSingleQuote: true,
+    tabWidth: 4,
+    printWidth: 100,
+    proseWrap: 'always',
+    arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+};
